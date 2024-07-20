1e8c2790d9046d8d446065a7731be95bcd50e262
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-07-20 13:20:32 +0530

    configs changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['t4.ftcdn.net','zos.alipayobjects.com'], // Add your image domains here
+    },
+};
 
 export default nextConfig;
